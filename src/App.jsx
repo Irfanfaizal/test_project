20352822794a1c974811c6bcd5e8f748edaf6431
Irfanfaizal <richurichu386@gmail.com>
2025-06-20 09:39:00 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello World!Welcome...
+          Hello World!Welcome...2025
         </p>
       </div>
       <p className="read-the-docs">
